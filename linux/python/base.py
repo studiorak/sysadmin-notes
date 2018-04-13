a0dd55e45850b73337cfc287d281ccc4dfdea6b9
Rakotovao <jeanluc.rakotovao@gmail.com>
2018-04-13 10:05:26 +0200

    Update base.py

--- linux/python/base.py
+++ linux/python/base.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 
 from optparse import OptionParser
+import os
+import pathlib
+import re
 
 parser = OptionParser()
 parser.add_option("-i", "--input-dir", dest="dir", help="input directory")
@@ -8,13 +11,38 @@ parser.add_option("-i", "--input-dir", dest="dir", help="input directory")
 
 input = options.dir
 
-class Test:
+class Vhost:
 
     def __init__(self):
+        input1 = input
         self.input = input
 
-    def display(self):
-        print(self.input)
+    def job(self, queueElem):
+        print(queueElem)
 
-instance = Test()
-instance.display()
+    def list(self):
+        return os.listdir(pathlib.Path('../', self.input))
+
+    def cleanLinks(self, fileList):
+        for file in fileList:
+            myPath = pathlib.Path('../', 'data', file)
+            if len(open(myPath).readlines(  )) <= 1:
+                os.remove(myPath)
+
+    def readFile(self, fileList):
+        for file in fileList:
+            fileContent = []
+            myPath = str(pathlib.Path('../', 'data', file))
+            content = open(myPath, 'r')
+            for line in content:
+                cleanedLine = re.sub(r'\s{2,200}', '', line)
+                cleanedLine = re.sub(r'<\W*>', '', line)
+                splitedLine = cleanedLine.strip().split()
+                for i in splitedLine:
+                    print(i)
+            content.close()
+
+vhost = Vhost()
+vhostList = vhost.list()
+vhost.cleanLinks(vhostList)
+vhostCat = vhost.readFile(vhostList)
